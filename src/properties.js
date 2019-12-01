04641826e454d456fe004b0b66da23264237f4ab
des williams <des_williams_2000@yahoo.com>
2019-12-01 23:06:52 +0100

    updated post for longs stage.

--- src/properties.js
+++ src/properties.js
@@ -2,6 +2,6 @@
 export const properties = {
     minm: "0",
     maxm: "9223372036854775807",
-    post: 'https://dvmkfdwpb1.execute-api.us-east-2.amazonaws.com/dev/conversion',
+    post: 'https://dvmkfdwpb1.execute-api.us-east-2.amazonaws.com/longs/conversion',
     log_out: true
 };
