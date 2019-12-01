a7dad89af9b830215d5a8f45a5a575f9a45a0c08
des williams <des_williams_2000@yahoo.com>
2019-12-01 22:47:49 +0100

    changed property maxm to maximum value of Long.

--- src/properties.js
+++ src/properties.js
@@ -1,7 +1,7 @@
 
 export const properties = {
     minm: "0",
-    maxm: "2147483647",
+    maxm: "9223372036854775807",
     post: 'https://dvmkfdwpb1.execute-api.us-east-2.amazonaws.com/dev/conversion',
     log_out: true
 };
